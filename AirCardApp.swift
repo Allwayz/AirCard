07add0b493837651f55faffa397124405317a980
Mak5er <mmaksym.reva@gmail.com>
2026-09-19 16:00:19 +0300

    chore(release): bump version to 1.2.3

--- AirCardApp.swift
+++ AirCardApp.swift
@@ -1706,7 +1706,7 @@ struct ContentView: View {
                     Text("AirCard")
                         .font(.title2)
                         .fontWeight(.bold)
-                    Text("v1.2.2")
+                    Text("v1.2.3")
                         .font(.system(size: 10, weight: .bold, design: .rounded))
                         .padding(.horizontal, 6)
                         .padding(.vertical, 2)
